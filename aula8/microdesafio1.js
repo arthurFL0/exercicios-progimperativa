914e3e9f9f8686000c726d9ac9f90b3a6e28e3e3
Arthur <arthurlacerda206@gmail.com>
2021-06-22 09:27:11 -0300

    "pequenas modificações"

--- aula8/microdesafio1.js
+++ aula8/microdesafio1.js
@@ -1,14 +1,16 @@
 // Crie uma função construtora chamada Restaurante, que receba como parâmetros o nome do restaurante e o cardápio (que será um array de strings). 
 // Após isso chame a função entrada() dentro do objeto, que irá trazer uma mensagem de boas-vindas, informando o nome do restaurante, e o cardápio.
 
-function Restaurante(nome,cardapio,){
+function Restaurante(nome,cardapio){
     this.nome = nome;
     this.cardapio = cardapio;
-    this.entrada =  entrada =()=> 'Seja bem vindo ao restaurante '+nome+'.'+' O cardapio de hoje é '+cardapio+'.'
+    this.entrada =  entrada =()=> 'Seja bem vindo ao restaurante '+nome+'.'+' O cardápio de hoje é '+cardapio+'.'
 }
 
 
 let callRestaurante = new Restaurante("California",["Pizza","Massas variadas","Lasanha"])
 
+let restaurante2 = new Restaurante("Fausto's Kitchen",["Ovo","Ovo cozido","Ovo frito","Ovo e ovo"])
 
 console.log(callRestaurante.entrada())
+console.log(restaurante2.entrada())
