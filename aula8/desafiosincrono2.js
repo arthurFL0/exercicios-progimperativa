b0fe7b183ea5217064d9f3a66293298b684b1664
Arthur <arthurlacerda206@gmail.com>
2021-06-26 12:28:58 -0300

    "comentários"

--- aula8/desafiosincrono2.js
+++ aula8/desafiosincrono2.js
@@ -6,14 +6,17 @@
 // Gostaríamos de calcular a média do aluno e se o aluno foi aprovado com base na nota de aprovação que será dada. Para este exercício, vamos deixar você pensar sobre todos os métodos e propriedades que podem ser necessários para que nosso programa funcione corretamente da maneira solicitada.
 
 // A média será 7
-var total= 0
 
+
+var total= 0
+// função para calcular a média
 function media (lista_notas) {
     for (var i = 0; i < lista_notas.lenght; i++){
         total += lista_notas[i]
   }
 }
 
+// O método desse objeto deveria retornar a função media e depois dividir pelo tamanho da array assim conseguido a média das notas.
 let aluno = {
     nome: 'Arthur',
     numero_arquivo: 22,
